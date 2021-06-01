5c5f4bd34b73656e4a9cfcb403a9fce281401666
007gzs <007gzs@gmail.com>
2021-06-01 11:26:17 +0800

    Bump version: 0.1.10 → 0.1.11

--- oface/__init__.py
+++ oface/__init__.py
@@ -4,4 +4,4 @@ from __future__ import absolute_import, unicode_literals
 from .face_analysis import FaceAnalysis, Face
 
 __all__ = ['FaceAnalysis', 'Face']
-__version__ = '0.1.10'
+__version__ = '0.1.11'
